b6d5e9ab2b134c7a55a34ce5764ce1db8b8326a6
Kevin Kuehler <kkuehler@berkeley.edu>
2017-11-24 02:02:10 -0800

    Style consistency

--- modules/ocf_ldap/manifests/init.pp
+++ modules/ocf_ldap/manifests/init.pp
@@ -94,10 +94,10 @@ class ocf_ldap {
   ocf::firewall::firewall46 {
     '101 allow ldaps':
       opts => {
-        'chain'  => 'PUPPET-INPUT',
-        'proto'  => [ 'tcp', 'udp' ],
-        'dport'  => 'ldaps',
-        'action' => 'accept',
+        chain  => 'PUPPET-INPUT',
+        proto  => ['tcp', 'udp'],
+        dport  => 'ldaps',
+        action => 'accept',
       };
   }
 }
